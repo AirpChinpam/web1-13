f576e697949a0a4212711cb2d17d840cca5d5737
AirpChinpam <rdkxr678@yahoo.co.jp>
2017-07-03 18:42:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -36,7 +36,7 @@ function setCookie(c_name,value,expiredays){
  if(last_date){
      document.getElementById('cookie').textContent = '前回訪れた:' + last_date;
  }else{
-    document.getElementById('cookie').textContent = はじめまして';
+    document.getElementById('cookie').textContent = 'はじめまして';
 }
  
  var current_time = new Date();
